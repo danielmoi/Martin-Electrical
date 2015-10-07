ba4a15f70701cbf065dde9ec4834bd2652efc463
dm4000 <daniel@listretto.com>
2015-10-08 08:56:01 +1100

    Implement nested media queries
    just for p for now

--- testimonials.php
+++ testimonials.php
@@ -13,7 +13,7 @@
       <div class="quote">
         <p>Great Service. The electrician came on time. He fixed and replaced everything I asked for correctly and quickly without any mess. The price was reasonable and well worth the service and quality provided. I will be recommending Martin Electrical and will use them again. Thanks lads.</p>
       </div>
-      
+
       <p class="name">Thomas Rangleman</p>
       <p class="date">13 July 2015</p>
     </article>
@@ -21,17 +21,17 @@
     <article class="testimonial">
       <div class="quote">
 
-      <p>Some of our recent clients reviewed us, and here's what they had to say!</p>
-      <p>We were extremely impressed with Sam's diligence and workmanship. He went above and beyond by removing residue around the exhaust fan in the laundry area. He tidied all areas well and took away all old materials for disposal. We would highly recommend Sam for any electrical work to other customers.</p>
+        <p>Some of our recent clients reviewed us, and here's what they had to say!</p>
+        <p>We were extremely impressed with Sam's diligence and workmanship. He went above and beyond by removing residue around the exhaust fan in the laundry area. He tidied all areas well and took away all old materials for disposal. We would highly recommend Sam for any electrical work to other customers.</p>
       </div>
-        <p class="name">Jennifer Ralph</p>
+      <p class="name">Jennifer Ralph</p>
       <p class="date">11 August 2015</p>
     </article>
 
     <article class="testimonial">
       <div class="quote">
 
-      <p>I was pleased with the rewiring work done on my house in May. The team was a great lot of young men and I will certainly use your services again if they are required. Many thanks.</p>
+        <p>I was pleased with the rewiring work done on my house in May. The team was a great lot of young men and I will certainly use your services again if they are required. Many thanks.</p>
       </div>
       <p class="name">Audrey Scarborough</p>
       <p class="date">04 September 2015</p>
@@ -40,7 +40,8 @@
     <article class="testimonial">
       <div class="quote">
 
-      <p>Just a short note on the great service I had from your company once again, and to give you feedback . I phoned just before 7am and my call was handled very professionally / in a friendly manner. The lady on the phone informed me that I would have an electrician asap. Within an hour there were two very happy professional electricians on the job and it took no time for them to sort out the problem. Thank you for a job well done. I will continue to recommend your company to others.</p>
+        <p>Just a short note on the great service I had from your company once again, and to give you feedback . I phoned just before 7am and my call was handled very professionally / in a friendly manner. The lady on the phone informed me that I would have an electrician asap.</p>
+        <p> Within an hour there were two very happy professional electricians on the job and it took no time for them to sort out the problem. Thank you for a job well done. I will continue to recommend your company to others.</p>
       </div>
       <p class="name">James Smith</p>
       <p class="date">10 October 2015</p>
