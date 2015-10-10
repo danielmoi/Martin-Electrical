7f25606d0ad2ad0581e252ef61d81c2f632392c6
dm4000 <daniel@listretto.com>
2015-10-10 12:01:03 +1100

    Clean up footer tag names

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 <footer>
   <div class="superbox">
-  <div class="box">
-    <div class="one"><p>&copy; 2015 Martin Electrical</p></div>
-    <div class="two"><img class="two_1" src="img/Twitter_logo_white_48.png"><img class="two_1" src="img/In-White-48px-R.png"></div>
+    <div class="box">
+      <div class="footer-text">
+        <p>&copy; 2015 Martin Electrical</p>
+      </div>
+      <div class="social"><img src="img/Twitter_logo_white_48.png"><img src="img/In-White-48px-R.png"></div>
 
-  </div>
+    </div>
   </div>
 </footer>
 <script src="js/script.js"></script>
