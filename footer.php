e534bac148f7f926f3b1f3510f35ea413492240a
dm4000 <daniel@listretto.com>
2015-10-10 11:52:52 +1100

    Align social icons in footer

--- footer.php
+++ footer.php
@@ -1,9 +1,13 @@
-  <footer>
-    <div class="footer">
-      <p>&copy; 2015 Martin Electrical</p>
-    </div>
-  </footer>
-  <script src="js/script.js"></script>
+<footer>
+  <div class="superbox">
+  <div class="box">
+    <div class="one"><p>&copy; 2015 Martin Electrical</p></div>
+    <div class="two"><img class="two_1" src="img/Twitter_logo_white_48.png"><img class="two_1" src="img/In-White-48px-R.png"></div>
+
+  </div>
+  </div>
+</footer>
+<script src="js/script.js"></script>
 </body>
 
 
