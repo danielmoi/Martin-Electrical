f5b2b9b1a9976cee4a90df4d18a17bf104771a91
dm4000 <daniel@listretto.com>
2015-10-15 10:10:21 +1100

    Fix miscellaneous
    Rename social, fix alignment.

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
       <div class="footer-text">
         <p>&copy; 2015 Martin Electrical</p>
       </div>
-      <div class="social"><img src="img/Twitter_logo_white_48.png"><img src="img/In-White-48px-R.png"></div>
+      <div class="social"><img src="img/twitter.png"><img src="img/linkedin.png"></div>
 
     </div>
   </div>
